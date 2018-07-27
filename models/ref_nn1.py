e62616ff18ea2aeb0509e6c0db4f9d121aa6b41d
evanthebouncy <evanthebouncy@gmail.com>
2018-07-26 22:18:38 -0700

    loss going down . . .

--- models/ref_nn1.py
+++ models/ref_nn1.py
@@ -81,6 +81,19 @@ class RefNN(nn.Module):
     val = self.mk_val(x)
     return key, val
 
+  # takes in a query, M keys, create weights
+  def make_W(self, qry, keys):
+    '''
+      qry :  [ batch x n_qry ]
+      keys : [ M x batch x n_qry ]
+    '''
+    M = keys.size()[0]
+    qry = qry.unsqueeze(0)
+    qry = qry.expand(M, -1, -1)
+    W = torch.sum(qry * keys, dim=2) # [ M x batch ] 
+    W = W  / W.norm(2, 0, keepdim=True) # [ M x batch ] normalized along axis 0
+    return W
+
   # takes in a qry, M keys and M values, produce a context based on the qry
   def make_ctx(self, qry, keys, vals):
     '''
@@ -88,11 +101,7 @@ class RefNN(nn.Module):
       keys : [ M x batch x n_qry ]
       vals : [ M x batch x n_val ]
     '''
-    M = keys.size()[0]
-    qry = qry.unsqueeze(0)
-    qry = qry.expand(M, -1, -1)
-    W = torch.sum(qry * keys, dim=2) # [ M x batch ] 
-    W = W  / W.norm(2, 0, keepdim=True) # [ M x batch ] normalized along axis 0
+    W = self.make_W(qry, keys) # [ M x batch ]
     W = W.unsqueeze(-1) # [ M x batch x 1 ]
     W = W.expand(-1, -1, self.n_val) # [ M x batch x n_val ]
     weighted_vals = W * vals # [ M x batch x n_val ]
@@ -129,17 +138,7 @@ class RefNN(nn.Module):
     pred = self.make_pred(ctx1, ctx2)
     return pred
 
-
-if __name__ == "__main__":
-  print ("hello we're about to crash")
-  ref_nn = RefNN(1, 28, 10).cuda()
-
-  from data.load_data import load_datas
-  from discovery_models.random_subset import RSub
-
-  data_name = "mnist"
-  # load the data
-  tr_img, tr_lab, t_img, t_lab = load_datas("./data/", data_name)
+def generate_ref_data(imgs, labs):
   # use the rsub for random subset for now
   r_sub = RSub()
 
@@ -147,7 +146,7 @@ if __name__ == "__main__":
   ref_imgs = []
   ref_labs = []
   for i in range(100):
-    img_batch, lab_batch = r_sub.get_subset(tr_img, tr_lab, 40)
+    img_batch, lab_batch = r_sub.get_subset(imgs, labs, 40)
     ref_imgs.append(img_batch)
     # convert the label to 1-hot here
     lab_batch = np.array(lab_batch)
@@ -160,14 +159,34 @@ if __name__ == "__main__":
   ref_labs = to_torch(ref_labs, "float").view(100, -1, 10)
 
   # generate img input
-  img_batch, lab_batch = r_sub.get_subset(tr_img, tr_lab, 40)
+  img_batch, lab_batch = r_sub.get_subset(imgs, labs, 40)
   img_batch = to_torch(np.array(img_batch), "float").view(-1, 1, 28, 28)
+  lab_batch = to_torch(np.array(lab_batch), "int")
 
-  # compute the output (THIS WILL CRASH SO HARD)
-  print ("prepare to crash")
-  pred = ref_nn(img_batch, ref_imgs, ref_labs)
+  return img_batch, ref_imgs, ref_labs, lab_batch
 
-  print (pred)
+
+if __name__ == "__main__":
+  print ("hello we're about to crash")
+  ref_nn = RefNN(1, 28, 10).cuda()
+
+  from data.load_data import load_datas
+  from discovery_models.random_subset import RSub
+
+  data_name = "mnist"
+  # load the data
+  tr_img, tr_lab, t_img, t_lab = load_datas("./data/", data_name)
+
+  for _ in range(1000):
+    # generate training batch
+    img_batch, ref_imgs, ref_labs, lab_batch = generate_ref_data(tr_img, tr_lab)
+    # optimize weights
+    ref_nn.opt.zero_grad()
+    pred = ref_nn(img_batch, ref_imgs, ref_labs)
+    loss = F.nll_loss(pred, lab_batch)
+    loss.backward()
+    ref_nn.opt.step()
+    print (loss)
 
 
 
